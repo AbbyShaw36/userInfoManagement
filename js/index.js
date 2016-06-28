b376617cb52d6035de23d9ad2a9b9f04d70c8019
abbyshaw <reading.bear36@gmail.com>
2016-06-28 18:14:25 +0800

    完成创建和删除功能

--- js/index.js
+++ js/index.js
@@ -1,8 +1,117 @@
+var InfoTable = React.createClass({displayName: "InfoTable",
+	render : function() {
+		return (
+			React.createElement("table", null, 
+				React.createElement("thead", null, 
+					React.createElement("tr", null, 
+						React.createElement("td", null, "序号"), 
+						React.createElement("td", null, "姓名"), 
+						React.createElement("td", null, "年龄"), 
+						React.createElement("td", null, "性别"), 
+						React.createElement("td", null, "操作")
+					)
+				), 
+				React.createElement(InfoList, {source: "info.json"})
+			)
+		);
+	}
+});
+
+var InfoList = React.createClass({displayName: "InfoList",
+	getInitialState : function() {
+		return {
+			data : []
+		};
+	},
+	componentDidMount : function() {
+		this.serverRequest = $.get(this.props.source,function(result) {
+			this.setState({
+				data : result
+			});
+		}.bind(this));
+
+		$("#formBtnBox input").on("click",function() {
+			var name = $("#nameBox input").val();
+			var age = $("#ageBox input").val();
+			var sex = $("#sexBox select").val();
+
+			if (!name) {
+				alert("姓名不能为空！");
+				return;
+			}
+
+			if (!age) {
+				alert("年龄不能为空！");
+				return;
+			}
+
+			if (!sex) {
+				alert("性别不能为空！");
+				return;
+			}
+
+			var data = [];
+
+			data.push({
+				name : name,
+				age : age,
+				sex : sex
+			})
+
+			this.state.data.forEach(function(result) {
+				data.push(result);
+			});
+
+			this.setState({
+				data : data
+			});
+		}.bind(this));
+	},
+	componentWillUnmount : function() {
+		this.serverRequest.abort();
+	},
+	handleClick : function(index) {
+		var data = [];
+
+		delete this.state.data[index];
+
+		this.state.data.forEach(function(result,index) {
+			data.push(result);
+		});
+
+		this.setState({
+			data : data
+		});
+	},
+	render : function() {
+		var data = this.state.data;
+		var rows = [];
+		var o = this;
+
+		data.forEach(function(result,index) {
+			rows.push(
+				React.createElement("tr", null, 
+					React.createElement("td", null, index + 1), 
+					React.createElement("td", null, result.name), 
+					React.createElement("td", null, result.age), 
+					React.createElement("td", null, result.sex), 
+					React.createElement("td", null, 
+						React.createElement("a", {href: "#"}, "修改"), 
+						React.createElement("a", {href: "#", onClick: o.handleClick.bind(this,index)}, "删除")
+					)
+				)
+			);
+		});
+
+		return (
+			React.createElement("tbody", null, 
+				rows
+			)
+		);
+	}
+});
+
 ReactDOM.render(
-	React.createElement("div", {id: "formBox"}),
-	document.getElementById("main")
+	React.createElement(InfoTable, null),
+	document.getElementById('infoTable')
 );
-
-var NameBox = function() {
-
-}
--- jsx/index.js
+++ jsx/index.js
@@ -1,17 +1,117 @@
-ReactDOM.render(
-	<div id="formBox"></div>,
-	document.getElementById("main")
-);
-
-var NameBox = function() {
+var InfoTable = React.createClass({
 	render : function() {
 		return (
-			<div>
-				<label>
-					<span></span>
-					<input type="text"  />
-				</label>
-			</div>
+			<table>
+				<thead>
+					<tr>
+						<td>序号</td>
+						<td>姓名</td>
+						<td>年龄</td>
+						<td>性别</td>
+						<td>操作</td>
+					</tr>
+				</thead>
+				<InfoList source="info.json"/>
+			</table>
 		);
 	}
-}
+});
+
+var InfoList = React.createClass({
+	getInitialState : function() {
+		return {
+			data : []
+		};
+	},
+	componentDidMount : function() {
+		this.serverRequest = $.get(this.props.source,function(result) {
+			this.setState({
+				data : result
+			});
+		}.bind(this));
+
+		$("#formBtnBox input").on("click",function() {
+			var name = $("#nameBox input").val();
+			var age = $("#ageBox input").val();
+			var sex = $("#sexBox select").val();
+
+			if (!name) {
+				alert("姓名不能为空！");
+				return;
+			}
+
+			if (!age) {
+				alert("年龄不能为空！");
+				return;
+			}
+
+			if (!sex) {
+				alert("性别不能为空！");
+				return;
+			}
+
+			var data = [];
+
+			data.push({
+				name : name,
+				age : age,
+				sex : sex
+			})
+
+			this.state.data.forEach(function(result) {
+				data.push(result);
+			});
+
+			this.setState({
+				data : data
+			});
+		}.bind(this));
+	},
+	componentWillUnmount : function() {
+		this.serverRequest.abort();
+	},
+	handleClick : function(index) {
+		var data = [];
+
+		delete this.state.data[index];
+
+		this.state.data.forEach(function(result,index) {
+			data.push(result);
+		});
+
+		this.setState({
+			data : data
+		});
+	},
+	render : function() {
+		var data = this.state.data;
+		var rows = [];
+		var o = this;
+
+		data.forEach(function(result,index) {
+			rows.push(
+				<tr>
+					<td>{index + 1}</td>
+					<td>{result.name}</td>
+					<td>{result.age}</td>
+					<td>{result.sex}</td>
+					<td>
+						<a href="#">修改</a>
+						<a href="#" onClick={o.handleClick.bind(this,index)}>删除</a>
+					</td>
+				</tr>
+			);
+		});
+
+		return (
+			<tbody>
+				{rows}
+			</tbody>
+		);
+	}
+});
+
+ReactDOM.render(
+	<InfoTable />,
+	document.getElementById('infoTable')
+);
